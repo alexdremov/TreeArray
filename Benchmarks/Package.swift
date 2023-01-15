b95626ebd0af2455b038b584b6b14a57befa13e7
alexdremov <dremov.me@gmail.com>
2023-01-15 21:39:50 +0300

    ManagedBuffer optimizations, inlines, fastrandom

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -1,15 +1,4 @@
 // swift-tools-version:5.7
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift Collections open source project
-//
-// Copyright (c) 2021 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-//
-//===----------------------------------------------------------------------===//
-
 import PackageDescription
 
 let package = Package(
@@ -18,8 +7,8 @@ let package = Package(
     .executable(name: "benchmark", targets: ["benchmark"])
   ],
   dependencies: [
-    .package(name: "TreeArray", path: ".."),
     .package(url: "https://github.com/apple/swift-collections-benchmark", from: "0.0.3"),
+    .package(name: "TreeArray", path: ".."),
   ],
   targets: [
     .target(
@@ -27,13 +16,12 @@ let package = Package(
       dependencies: [
         .product(name: "CollectionsBenchmark", package: "swift-collections-benchmark"),
         .product(name: "TreeArray", package: "TreeArray")
-      ],
-      path: "Sources/Benchmarks"
+      ]
     ),
     .executableTarget(
       name: "benchmark",
       dependencies: [
-        .target(name: "Benchmarks")
+        "Benchmarks"
       ],
       path: "Sources/benchmark-tool"
     )
---
+++ Sources/TreeArray/TreeArray+Memory.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alex Dremov on 15.01.2023.
+//
+
+import Foundation
